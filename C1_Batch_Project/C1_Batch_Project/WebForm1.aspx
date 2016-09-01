75b7e0cd48437e3fdea2e7a71c012d27b367e9a1
mehul <mehulbudasna23@gmail.com>
2016-09-01 18:25:56 +0530

    Mehul

---
+++ C1_Batch_Project/C1_Batch_Project/WebForm1.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication12.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+             <td>
+    <asp:Label ID="labelcolor" runat="server">choose color for background:</asp:Label>
+        <br />
+        <asp:Panel ID="controlpanel" runat="server" BorderStyle="Inset">
+        <asp:DropDownList ID="backgroundcolor" runat="server" OnSelectedIndexChanged="backgroundcolor_SelectedIndexChanged" AutoPostBack="True">
+            <asp:ListItem>Yellow</asp:ListItem>
+            <asp:ListItem>blue</asp:ListItem>
+            <asp:ListItem>green</asp:ListItem>
+            <asp:ListItem>red</asp:ListItem>
+            <asp:ListItem>grey</asp:ListItem>
+            <asp:ListItem>black</asp:ListItem>
+             </asp:DropDownList>
+            <br /> <br />
+
+            <asp:Label ID="fontsize" runat="server">Choose font size:</asp:Label>
+            <br />
+         <asp:DropDownList ID="DropDownList" runat="server" >
+            <asp:ListItem>small</asp:ListItem>
+            <asp:ListItem>meduim</asp:ListItem>
+            <asp:ListItem>large</asp:ListItem>
+            <asp:ListItem>xlarge</asp:ListItem>
+            <asp:ListItem>xsmall</asp:ListItem>
+            </asp:DropDownList>
+    
+            <br /><br />
+            <asp:Label ID="message1"  runat="server"></asp:Label>
+           <br />
+             <asp:TextBox Height="70" Width="270" ID="message" runat="server">
+            </asp:TextBox>
+            <br /><br />
+            <asp:Button ID="update"  runat="server" Text="Update" OnClick="update_Click" />
+    </asp:Panel>
+            </td>
+                <td></td>
+            <td>
+                <asp:Panel ID="output" Height="370" Width="270"  runat="server" > 
+                    <br /> <br />
+                    <div style="margin:0px 0px 0px 70px">
+                        <asp:Label ID="outputmessage"  Text="Happy BirthDay" runat="server"> Happy Birthday </asp:Label>         
+                
+                          <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <div style="margin:0px 0px 0px -30px">
+                               <asp:Image ID="img" Width="200px" Height="157px" ImageUrl="~/images.jpg" runat="server" />                
+                        </div>
+                    </div>  
+                </asp:Panel>
+                </td>
+
+                </tr>
+            </table>
+   </div>
+    </form>
+</body>
+</html>
---
+++ C1_Batch_Project/C1_Batch_Project/WebForm1.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+namespace WebApplication12
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String color = backgroundcolor.SelectedValue;
+            if (color == "Yellow")
+            {
+                output.BackColor = System.Drawing.Color.Yellow;
+            }
+        }
+
+        protected void backgroundcolor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String color = backgroundcolor.SelectedValue;
+            if (color == "Yellow")
+            {
+                output.BackColor = System.Drawing.Color.Yellow;
+            }
+            else if (color == "green")
+            {
+                output.BackColor = System.Drawing.Color.Green;
+            }
+            else if (color == "black")
+            {
+                output.BackColor = System.Drawing.Color.Black;
+            }
+            else if (color == "blue")
+            {
+                output.BackColor = System.Drawing.Color.Blue;
+            }
+            else if (color == "green")
+            {
+                output.BackColor = System.Drawing.Color.Green;
+            }
+            else if (color == "red")
+            {
+                output.BackColor = System.Drawing.Color.Red;
+            }
+            else if (color == "grey")
+            {
+                output.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+            String sizefont = DropDownList.SelectedValue;
+            if (sizefont == "small")
+            {
+                outputmessage.Font.Size = FontUnit.Small;
+            }
+            if (sizefont == "meduim")
+            {
+                outputmessage.Font.Size = FontUnit.Medium;
+            }
+            if (sizefont == "large")
+            {
+                outputmessage.Font.Size = FontUnit.Large;
+            }
+            if (sizefont == "xlarge")
+            {
+                outputmessage.Font.Size = FontUnit.Larger;
+            }
+            if (sizefont == "xsmall")
+            {
+                outputmessage.Font.Size = FontUnit.Smaller;
+            }
+
+            String mess = message.Text;
+            if (mess == "")
+            {
+                outputmessage.Text = "Happpy BirthDay";
+                //            outputmessage.Text = mess; 
+            }
+            else {
+                //  outputmessage.Text = "Happpy BirthDay";
+                outputmessage.Text = mess;
+            }
+        }
+
+       
+
+    }
+}
